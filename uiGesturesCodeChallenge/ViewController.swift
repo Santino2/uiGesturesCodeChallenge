4fcf49dbc5c8e6fc15f4c0e001ca54c211be013c
SANTINO CIRELLI <rl200522@rlas-macair-31.rlas-116.org>
2018-11-09 10:45:22 -0600

    Initial Commit

---
+++ uiGesturesCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uiGesturesCodeChallenge
+//
+//  Created by SANTINO CIRELLI on 11/9/18.
+//  Copyright © 2018 SANTINO CIRELLI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
